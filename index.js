59914cf9d6ea5f23b0f6d360010f5c4fad168229
Mark Shkreli <m.shkreli96@icloud.com>
2022-09-14 14:46:10 -0400

    commit tree 68c223900257a0ebd61dbbcb5808c4f088e8751a

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+};
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+};
+
+function destructivelyRemoveLastCat() {
+    cats.pop(cats.length - 1);
+};
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(0);
+};
+
+function appendCat(newCats){
+    newCats = [...cats, "Broom"];
+    return newCats;
+};
+
+function prependCat(newCats) {
+    newCats = ["Arnold", ...cats];
+    return newCats;
+};
+
+function removeLastCat(newCats) {
+    newCats = cats.slice(0,2);
+    return newCats;
+};
+
+function removeFirstCat(newCats) {
+    newCats = cats.slice(1);
+    return newCats;
+};
